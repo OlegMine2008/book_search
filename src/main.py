6116c67ce170822b0dc01519376de37de8b76cb8
OlegMine2008 <olegkosh08@gmail.com>
2025-11-23 10:12:11 +0300

    Redacted with some code

--- src/main.py
+++ src/main.py
@@ -1,12 +1,24 @@
-from data.book_searcher_ui import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow, QApplication
+import sqlite3
+import sys
+
+from book_searcher_ui import Ui_MainWindow
+from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog
 
 
 class Book_Search(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
+        self.con = None
+        self.db_connect.clicked.connect(self.db_connection)
+        self.search_method.clicked.connect(self.search)
+    
+    def db_connection(self):
+        db_name = QFileDialog.getOpenFileName(self, 'Выберите базу данных', '', '(*.db);;(*.sqlite3)')[0]
+        self.con = sqlite3.connect(db_name)
+    
+    def search(self):
+        pass
 
 if __name__ == "__main__":
     app = QApplication([])
