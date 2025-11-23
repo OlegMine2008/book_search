c6df4975d1c0feaaeec0924211d4c0b08ab52a81
OlegMine2008 <olegkosh08@gmail.com>
2025-11-23 10:28:05 +0300

    проблемы с мейном - разобраться с поиском

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ import sqlite3
 import sys
 
 from book_searcher_ui import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QInputDialog
+from PyQt6.QtSql import QSqlQueryModel
 
 
 class Book_Search(QMainWindow, Ui_MainWindow):
@@ -17,8 +18,20 @@ class Book_Search(QMainWindow, Ui_MainWindow):
         db_name = QFileDialog.getOpenFileName(self, 'Выберите базу данных', '', '(*.db);;(*.sqlite3)')[0]
         self.con = sqlite3.connect(db_name)
     
-    def search(self):
-        pass
+    # def search(self):
+    #     cur = self.con.cursor()
+    #     methode, ok_pressed = QInputDialog.getItem(self, "Поиск", "Выберите метод поиска", ("Код", "Название", "Автор"), 1, False)
+    #     if ok_pressed and methode == 'Код':
+    #         info, new_pressed = QInputDialog.getText(self, "Поиск по коду", "Введите код книги")
+    #         if new_pressed:
+    #             model = QSqlQueryModel()
+    #             model.setQuery('SELECT * FROM ')
+    #             self.tableView.setModel(f'SELECT * FROM {}')
+    #     elif ok_pressed and methode == 'Название':
+    #         info, new_pressed = QInputDialog.getText(self, "Поиск по названию", "Введите название книги")
+    #     elif ok_pressed and methode == 'Автор':
+    #         info, new_pressed = QInputDialog.getText(self, "Поиск по автору", "Введите имя автора")
+
 
 if __name__ == "__main__":
     app = QApplication([])
