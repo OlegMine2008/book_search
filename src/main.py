fb7d32fd671a89316fae02657162dbe104803af9
OlegMine2008 <olegkosh08@gmail.com>
2025-11-30 13:27:38 +0300

    READY FOR ARRIVAL

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import sys
 
 from book_searcher_ui import Ui_MainWindow
 from asking_ui import Ui_Form
+from adding_with_new_ui import Ui_Dialog
 from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QDialog, QMessageBox, QInputDialog
 from PyQt6.QtSql import QSqlQueryModel
 
@@ -14,15 +15,34 @@ class Book_Search(QMainWindow, Ui_MainWindow):
         self.con = None
         self.db_connect.clicked.connect(self.db_connection)
         # self.search_method.clicked.connect(self.search)
-        # self.add_book.clicked.connect(self.new_book)
+        self.add_book.clicked.connect(self.new_book)
         self.update_info.clicked.connect(self.update_infor)
 
     def db_connection(self):
         db_name = QFileDialog.getOpenFileName(self, 'Выберите базу данных', '', '(*.db);;(*.sqlite3)')[0]
         self.con = sqlite3.connect(db_name)
     
-    # def new_book(self):
-    #     cur = self.con.cursor()
+    def new_book(self):
+        if not self.con:
+            QMessageBox.warning(self, "Ошибка", "Сначала подключите базу данных")
+            return
+        
+        cur = self.con.cursor()
+        author_id = cur.execute('''SELECT author_id, (SELECT author_name FROM authors WHERE id == author_id) 
+                                FROM books''').fetchall()
+        
+        if not author_id:
+            QMessageBox.warning(self, "Ошибка", "База данных пуста")
+            return
+        
+        author_id = [f"{author_cur_id}, {name}" for author_cur_id, name in author_id]
+        author_id.append('Новый автор')
+        author, ok_pressed = QInputDialog.getItem(self, "Выбор", "Выберите автора или введите нового", author_id, 0, False)
+        if ok_pressed:
+            if author == 'Новый автор':
+                u = Adding_Window_new(cur, self)
+                u.exec()
+                self.con.commit()
 
     # def search(self):
     #     cur = self.con.cursor()
@@ -52,9 +72,8 @@ class Book_Search(QMainWindow, Ui_MainWindow):
         if not books_data:
             QMessageBox.warning(self, "Ошибка", "База данных пуста")
             return
-    
+        
         book_items = [f"{book_id}, {book_name}, {author}" for book_id, book_name, author in books_data]
-    
         book, ok_pressed = QInputDialog.getItem(
             self, "Выбор", "Выберите книгу", book_items, 0, False
         )
@@ -91,7 +110,33 @@ class Asking_Window(QDialog, Ui_Form):
                     (info_result, notes_result, self.book_id))
             
         self.accept()
+
+
+class Adding_Window_new(QDialog, Ui_Dialog):
+    def __init__(self, cursor, main=None):
+        super().__init__(main)
+        self.setupUi(self)
+        self.cur = cursor
         
+        self.sureButton.clicked.connect(self.approve)
+                
+    
+    def approve(self):
+        name = self.author_name.toPlainText()
+        book = self.book_name.toPlainText()
+        info_result = self.information.toPlainText()
+        notes_result = self.notes.toPlainText()
+        
+        self.cur.execute('''INSERT INTO authors(author_name)
+                         VALUES(?)''', 
+                    (name,))
+        self.cur.execute('''INSERT INTO books(book_name, info, notes, author_id)
+                         VALUES(?, ?, ?, (SELECT id FROM authors
+                         WHERE author_name == ?))''', 
+                    (book, info_result, notes_result, name))
+            
+        self.accept()
+
 
 if __name__ == "__main__":
     app = QApplication([])
